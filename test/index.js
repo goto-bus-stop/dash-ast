344e622d4ca0b296cc20b4142d420e3c9d4693a9
Renée Kooi <renee@kooi.me>
2019-07-11 14:18:45 +0200

    Update dependencies.

--- test/index.js
+++ test/index.js
@@ -3,7 +3,7 @@ var src = require('fs').readFileSync(require.resolve('acorn'))
 var parse = require('acorn').parse
 var dash = require('../')
 
-var NUM_NODES = 25426
+var NUM_NODES = 26251
 
 test('dash-ast', function (t) {
   var ast = parse(src)
