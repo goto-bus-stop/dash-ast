85556b9736a79813b3627f43b9a9c75b26486d4e
Renée Kooi <renee@kooi.me>
2020-03-04 10:25:23 +0100

    Add estree-walker with both enter/leave to benchmark

--- bench/index.js
+++ bench/index.js
@@ -1,9 +1,9 @@
 var bench = require('nanobench')
+var semver = require('semver')
 var src = require('fs').readFileSync(require.resolve('acorn'))
 var parse = require('acorn').parse
 var astw = require('astw')
 var eswalk = require('estree-walk')
-var eswalker = require('estree-walker')
 var dash = require('../')
 
 bench('astw', function (b) {
@@ -98,22 +98,49 @@ bench('dash-ast with enter/leave', function (b) {
   b.end('walked ' + [i, j] + ' nodes')
 })
 
-bench('estree-walker', function (b) {
-  var ast = parse(src)
-  // warmup
-  var i = 0
-  for (var x = 0; x < 5; x++) {
-    eswalker.walk(ast, {
-      enter: function (node) { i++ }
-    })
-  }
-  i = 0
+if (semver.satisfies(process.version, '>= 8.0.0')) {
+  var eswalker = require('estree-walker')
 
-  b.start()
-  for (x = 0; x < 5; x++) {
-    eswalker.walk(ast, {
-      enter: function (node) { i++ }
-    })
-  }
-  b.end('walked ' + i + ' nodes')
-})
+  bench('estree-walker', function (b) {
+    var ast = parse(src)
+    // warmup
+    var i = 0
+    for (var x = 0; x < 5; x++) {
+      eswalker.walk(ast, {
+        enter: function (node) { i++ }
+      })
+    }
+    i = 0
+
+    b.start()
+    for (x = 0; x < 5; x++) {
+      eswalker.walk(ast, {
+        enter: function (node) { i++ }
+      })
+    }
+    b.end('walked ' + i + ' nodes')
+  })
+
+  bench('estree-walker with enter/leave', function (b) {
+    var ast = parse(src)
+    // warmup
+    var i = 0
+    var j = 0
+    for (var x = 0; x < 5; x++) {
+      eswalker.walk(ast, {
+        enter: function (node) { i++ },
+        leave: function (node) { j++ }
+      })
+    }
+    i = 0
+
+    b.start()
+    for (x = 0; x < 5; x++) {
+      eswalker.walk(ast, {
+        enter: function (node) { i++ },
+        leave: function (node) { j++ }
+      })
+    }
+    b.end('walked ' + [i, j] + ' nodes')
+  })
+}
